1af3f33fb29062288d264e96deaf171708f030f6
joykare <warugujoy@gmail.com>
2018-03-05 18:24:09 +0300

    (feat): add redux onto front end

--- client/index.js
+++ client/index.js
@@ -1,5 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import reducers from "./reducers";
 import App from "./components/App.js";
 
-ReactDOM.render(<App />, document.getElementById("app"));
+let configStore = applyMiddleware(thunk)(createStore);
+
+let store = configStore(reducers);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>, document.getElementById("app")
+);
